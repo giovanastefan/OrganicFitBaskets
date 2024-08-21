949a4c7a14c703273da295603b67b04ede25d7e2
Giovana Stefan <giovanstefan3@gmail.com>
2024-08-21 19:23:27 -0300

    Ajuste footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,21 @@
 import "./Footer.css";
 
 export const Footer = () => {
-    return (
-        <div className="footer-container">
-            <div className="footer">
-                <img src="../../../images/Logo-white.png" width="198px" height="40px" class="footer-logo" alt="OrganicFit" />
-                <div class="footer-text-container">
-                    <span class="footer-text">Natural Organics. All rights reserved.</span>
-                    <span class="footer-text">2024</span>
-                </div>
-            </div>
+  return (
+    <div className="footer-container">
+      <div className="footer">
+        <div class="footer-text-container">
+          <img
+            src="../../../images/Logo-white.png"
+            width="198px"
+            height="40px"
+            class="footer-logo"
+            alt="OrganicFit"
+          />
+          <p class="footer-text">Natural Organics. All rights reserved.</p>
+          <p class="footer-text">2024</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
