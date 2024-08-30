610ddd7ad4bb9feafaefe57ec83fad8884ed3c77
Giovana Stefan <giovanstefan3@gmail.com>
2024-08-29 23:33:00 -0300

    Final ajustes

--- src/context/Cart/Cart.jsx
+++ src/context/Cart/Cart.jsx
@@ -1,12 +1,24 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (productId) => {
-    setCartItems((prevItems) => [...prevItems, productId]);
+  const addToCart = ({ productId, quantity }) => {
+    setCartItems((prevItems) => {
+      const itemExists = prevItems.find((item) => item.productId === productId);
+
+      if (itemExists) {
+        return prevItems.map((item) =>
+          item.productId === productId
+            ? { ...item, quantity: item.quantity + quantity }
+            : item
+        );
+      }
+
+      return [...prevItems, { productId, quantity }];
+    });
   };
 
   return (
